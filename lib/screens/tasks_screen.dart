0758d0c5b090d57f07941ee6f1751ea931221624
cromueloliver02 <cromuelbarut@gmail.com>
2022-10-31 18:17:42 +0800

    made tasks count dynamic

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_app/models/task.dart';
 import '../blocs/blocs.dart';
 import '../widgets/task_list.dart';
 import '../widgets/add_task_modal.dart';
@@ -16,40 +17,42 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TaskBloc, TaskState>(
-      builder: (ctx, state) {
-        final tasks = state.tasks;
-
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Tasks App'),
-            actions: [
-              IconButton(
-                onPressed: () => _showAddTask(context),
-                icon: const Icon(Icons.add),
-              )
-            ],
-          ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Center(
-                child: Chip(
-                  label: Text('Tasks:'),
-                ),
-              ),
-              Expanded(
-                child: TaskList(tasks: tasks),
-              ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tasks App'),
+        actions: [
+          IconButton(
             onPressed: () => _showAddTask(context),
-            tooltip: 'Add Task',
-            child: const Icon(Icons.add),
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Chip(
+              label: BlocSelector<TaskBloc, TaskState, List<Task>>(
+                selector: (state) => state.tasks,
+                builder: (context, tasks) => Text('Tasks: ${tasks.length}'),
+              ),
+            ),
           ),
-        );
-      },
+          Expanded(
+            child: BlocSelector<TaskBloc, TaskState, List<Task>>(
+              selector: (state) => state.tasks,
+              builder: (context, tasks) {
+                return TaskList(tasks: tasks);
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showAddTask(context),
+        tooltip: 'Add Task',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
